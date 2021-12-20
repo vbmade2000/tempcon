bd643316a37756a613d43f9b16518fe9ce2fc1eb
Malhar Vora <mlvora.2010@gmail.com>
2021-12-20 21:47:57 +0530

    Make app stateful

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,17 @@ void main() {
   return runApp(TempConApp());
 }
 
-class TempConApp extends StatelessWidget {
+class TempConApp extends StatefulWidget {
   const TempConApp({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return TempConAppState();
+  }
+}
+
+class TempConAppState extends State<TempConApp> {
+  var from = "c";
 
   @override
   Widget build(BuildContext context) {
@@ -20,33 +29,37 @@ class TempConApp extends StatelessWidget {
             children: [
               Padding(
                 padding:
-                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
-                child: Row(
-                  children: [
-                    Flexible(
-                      child: TextField(
-                        autofocus: true,
-                        decoration: const InputDecoration(
-                            border: UnderlineInputBorder(),
-                            hintText: "Celsius"),
-                      ),
-                    ),
-                  ],
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10.0),
+                child: TextField(
+                  autofocus: true,
+                  decoration: const InputDecoration(
+                      border: UnderlineInputBorder(), hintText: "Celsius"),
                 ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_downward),
+                    onPressed: () {
+                      print("First button");
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.arrow_upward_sharp),
+                    onPressed: () {
+                      print("Second button");
+                    },
+                  ),
+                ],
               ),
               Padding(
                 padding:
-                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
-                child: Row(
-                  children: [
-                    Flexible(
-                      child: TextField(
-                        decoration: const InputDecoration(
-                            border: UnderlineInputBorder(),
-                            hintText: "Fahrenheit"),
-                      ),
-                    ),
-                  ],
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10.0),
+                child: TextField(
+                  decoration: const InputDecoration(
+                      border: UnderlineInputBorder(), hintText: "Fahrenheit"),
                 ),
               )
             ],
