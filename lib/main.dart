fe78d243146b010c390bcef1ca4e897aa4a20e39
Malhar Vora <mlvora.2010@gmail.com>
2021-12-20 22:54:58 +0530

    Add actual functionality with validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:string_validator/string_validator.dart" as validator;
 
 void main() {
   return runApp(TempConApp());
@@ -15,9 +16,17 @@ class TempConApp extends StatefulWidget {
 
 class TempConAppState extends State<TempConApp> {
   var from = "c";
+  var celsius = 0.0;
+  var fahrenheit = 0.0;
+
+  var cTextController = TextEditingController();
+  var fTextController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    this.cTextController.text = 0.toString();
+    this.fTextController.text = 0.toString();
+
     return MaterialApp(
       title: "Temperature Converter",
       home: Scaffold(
@@ -31,9 +40,16 @@ class TempConAppState extends State<TempConApp> {
                 padding:
                     const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10.0),
                 child: TextField(
+                  controller: this.cTextController,
                   autofocus: true,
                   decoration: const InputDecoration(
-                      border: UnderlineInputBorder(), hintText: "Celsius"),
+                      border: UnderlineInputBorder(),
+                      hintText: "Celsius",
+                      labelText: "Celsius"),
+                  onSubmitted: (data) {
+                    this.celsius = double.parse(data);
+                    this.cTextController.text = double.parse(data).toString();
+                  },
                 ),
               ),
               Row(
@@ -43,13 +59,25 @@ class TempConAppState extends State<TempConApp> {
                   IconButton(
                     icon: Icon(Icons.arrow_downward),
                     onPressed: () {
-                      print("First button");
+                      if (!validator.isFloat(this.cTextController.text)) {
+                        print("Value must be a numerical");
+                        return;
+                      }
+                      this.fTextController.text =
+                          ((double.parse(this.cTextController.text) * 1.8) + 32)
+                              .toString();
                     },
                   ),
                   IconButton(
                     icon: Icon(Icons.arrow_upward_sharp),
                     onPressed: () {
-                      print("Second button");
+                      if (!validator.isFloat(this.fTextController.text)) {
+                        print("Value must be a numerical");
+                        return;
+                      }
+                      var t1 = double.parse(this.fTextController.text) - 32;
+                      var t2 = (t1 * (5 / 9)).toString();
+                      this.cTextController.text = t2.toString();
                     },
                   ),
                 ],
@@ -58,8 +86,15 @@ class TempConAppState extends State<TempConApp> {
                 padding:
                     const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10.0),
                 child: TextField(
+                  controller: fTextController,
                   decoration: const InputDecoration(
-                      border: UnderlineInputBorder(), hintText: "Fahrenheit"),
+                      border: UnderlineInputBorder(),
+                      hintText: "Fahrenheit",
+                      labelText: "Fahrenheit"),
+                  onSubmitted: (data) {
+                    this.fahrenheit = double.parse(data);
+                    this.fTextController.text = double.parse(data).toString();
+                  },
                 ),
               )
             ],
